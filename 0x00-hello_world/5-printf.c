d7d3d36d80a461e73a02b8526ad98b1848e88e32
josfam <josephamukun@tutanota.com>
2023-09-07 17:45:32 -0700

    Add code for `5-printf.c`

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a partial quote/line
+ *
+ * Description: Prints a partial quote/line of some sort
+ *
+ * Return: 0 if successful. Non-zero otherwise.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
