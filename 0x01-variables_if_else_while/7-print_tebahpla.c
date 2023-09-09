40bb78e6e7fa1318b078d85fe947b1352828e58e
josfam <josephamukun@tutanota.com>
2023-09-08 19:47:10 -0700

    Add script for `7. Smile in the mirror`

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The entry point for this program.
+ *
+ * Return: 0 if successful. Non-zero otherwise.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+
+	return (0);
+}
