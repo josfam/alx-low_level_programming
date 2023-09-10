8dce75a751f7169d9d432039fa98ab08d971d751
josfam <josephamukun@tutanota.com>
2023-09-10 07:09:58 -0700

    Add C file for `10. Inventing is a combination...`

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The entry point for this program.
+ *
+ * Return: 0 if successful. Non-zero otherwise.
+ */
+int main(void)
+{
+	int left_num;
+	int right_num;
+	
+	/* For the left number */
+	for (left_num = 48; left_num <= 56; left_num++)
+	{
+		/* For the right number */
+		for (right_num = left_num; right_num <= 57; right_num++)
+		{
+			/* don't print the same numbers */
+			if (left_num != right_num)
+			{
+				putchar(left_num);
+				putchar(right_num);
+				if (left_num == 56 && right_num == 57)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
