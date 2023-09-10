05deb04b4ea755f96d190cc23301aaa7f703499b
josfam <josephamukun@tutanota.com>
2023-09-10 04:31:20 -0700

    Add C program for `8. UNIX is basically a , bu...`

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The entry point for this program.
+ *
+ * Return: 0 if successful. Non-zero otherwise.
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
