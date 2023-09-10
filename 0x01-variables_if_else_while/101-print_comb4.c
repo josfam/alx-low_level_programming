c132ee397de0780848c84c4acec78dd66526336d
josfam <josephamukun@tutanota.com>
2023-09-10 11:33:21 -0700

    Add program for `11. The success combination ...`

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The entry point for this program.
+ *
+ * Return: 0 if successful. Non-zero otherwise.
+ */
+int main(void)
+{
+	int left;
+	int mid;
+	int right;
+
+	/* for left digit */
+	for (left = '0'; left <= '7'; left++)
+	{
+		/* for middle digit */
+		for (mid = left + 1; mid <= '8'; mid++)
+		{
+			/* for right digit */
+			for (right = mid + 1; right <= '9'; right++)
+			{
+				/* print the three numbers*/
+				putchar(left);
+				putchar(mid);
+				putchar(right);
+				/* add the comma and space */
+				 /* but not after the last digit in the print*/
+				if (left == '7' && mid == '8' && right == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	/* print the newline */
+	putchar('\n');
+	return (0);
+}
