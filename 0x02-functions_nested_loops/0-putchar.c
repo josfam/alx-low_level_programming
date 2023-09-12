f1fbc6df6f813fa5da489a641b47f13c2aa9227d
josfam <josephamukun@tutanota.com>
2023-09-12 14:26:01 -0700

    Add code for `0. _putchar`

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The entry point for this program.
+ *
+ * Return: 0 if successful. Non-zero otherwise.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
