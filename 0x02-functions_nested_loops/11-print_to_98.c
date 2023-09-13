3c7e3b1ebd2796e698043eecbece2e933cf0f368
josfam <josephamukun@tutanota.com>
2023-09-13 15:16:57 -0700

    Fix Betty documentation errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,20 +3,21 @@
 /**
  * print_to_98 - Prints all natural numbers from n to 98.
  * @n: The number to start counting from.
- * Description: Prints all natural numbers from n to 98, followed by a new line. 
- * Return: Nothing. 
+ * Description: Prints all natural numbers from n to 98,
+ *				followed by a new line.
+ * Return: Nothing.
  */
 void print_to_98(int n)
 {
 	int delta;
 
-	/* decide whether to increment or decrement */	
+	/* decide whether to increment or decrement */
 	if (n > 98)
 		delta = -1;
 	else
 		delta = 1;
 
-	while(n != 98)
+	while (n != 98)
 	{
 		printf("%d, ", n);
 		n += delta;
