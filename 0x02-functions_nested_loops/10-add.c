05ab0bbdd9c8550b205a5f4a6d54e7e067f3d9ce
josfam <josephamukun@tutanota.com>
2023-09-13 13:33:06 -0700

    Add implementation for `add`

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result.
+ * @a: The first integer to add.
+ * @b: The second integer to add.
+ * Description: Adds two integers and returns the result.
+ * Return: The sum of the numbers a and b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
