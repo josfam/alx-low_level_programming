f6aaf2752abba34c8e6e562f0dbfd556db3aeded
josfam <josephamukun@tutanota.com>
2023-09-14 16:53:24 -0700

    Add main.h to new directory

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,44 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+/* prints a char to the screen*/
+void _putchar(char ch);
+
+/* prints the lowercase alphabet from a-z */
+void print_alphabet(void);
+
+/* prints the alphabet in lowercase 10 times */
+void print_alphabet_x10(void);
+
+/* checks whether a character is lowercase */
+int _islower(int c);
+
+/* checks for an alphabetical character */
+int _isalpha(int c);
+
+/* prints the sign of a number */
+int print_sign(int n);
+
+/* Computes the absoulute value of an integer */
+int _abs(int);
+
+/* Prints the last digit of a number */
+int print_last_digit(int);
+
+/* Prints every minute of the day of Jack Bauer */
+void jack_bauer(void);
+
+/* Prints the 9 times table starting from 0 */
+void times_table(void);
+
+/* Adds two integers and returns the result */
+int add(int, int);
+
+/* Prints all natural numbers from n to 98  */
+void print_to_98(int n);
+
+/* checks for a digit (0 through 9) */
+int _isdigit(int c);
+
+#endif /* MAIN_H */
+
