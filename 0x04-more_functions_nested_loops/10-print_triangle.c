248f5de1ff32ec482e70586eee87a4d6edda72a7
josfam <josephamukun@tutanota.com>
2023-09-15 01:07:16 -0700

    Add implementation for `print_triangle`

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle.
+ * @size: Size of the triangle.
+ * Description: Prints a triangle, followed by a new line.
+ * Return: Nothing.
+ */
+void print_triangle(int size)
+{
+	int row;
+	int spaces;
+	int hashes;
+
+	for (row = 0; row < size; row++)
+	{
+		/* determine spaces and hashes required for this row */
+		spaces = size - (row + 1);
+		hashes = row + 1;
+
+		/* print the spaces and reset them when finished */
+		while (spaces > 0)
+		{
+			_putchar(' ');
+			spaces--;
+		}
+
+		/* print the hashes and reset them when finished */
+		while (hashes > 0)
+		{
+			_putchar('#');
+			hashes--;
+		}
+
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+
