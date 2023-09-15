dfa54d05b8ed78fc3386ea64171561db5654d73b
josfam <josephamukun@tutanota.com>
2023-09-14 17:26:38 -0700

    Add implementation for `mul`

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer to multiply.
+ * @b: The second integer to multiply.
+ * Description: Multiplies two integers.
+ * Return: The result of multiplying a and b together.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
