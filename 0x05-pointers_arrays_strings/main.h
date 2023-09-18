1fd232dc86c03f70210aef8aa24241c2a7702ef4
josfam <josephamukun@tutanota.com>
2023-09-18 07:44:17 -0700

    Add signature for `void reset_to_98`

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,11 @@
 #ifndef MY_HEADER_H
 #define MY_HEADER_H
 
-/* prints a char to the screen*/
+/* Prints a char to the screen. */
 void _putchar(char ch);
+
+/* Takes a pointer to an int and updates the value it points to to 98. */
+void reset_to_98(int *n);
 
 #endif /* MAIN_H */
 
