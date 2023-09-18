99c6518359f8eaa493fcb7ae0636f6cfdb3a045d
josfam <josephamukun@tutanota.com>
2023-09-18 07:54:23 -0700

    Add implementation of `reset_to_98`

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer and updates the value it points to 98.
+ * @n: The pointer whose value to update to 98.
+ * Description: Takes a pointer and updates the value it points to 98.
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
