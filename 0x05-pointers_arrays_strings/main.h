94ab531d91db502e2d55f1fc9386b990f270a555
josfam <josephamukun@tutanota.com>
2023-09-18 07:59:19 -0700

    Add signature for `swap_int`

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,8 @@ void _putchar(char ch);
 /* Takes a pointer to an int and updates the value it points to to 98. */
 void reset_to_98(int *n);
 
+/* Swaps the values of two integers. */
+void swap_int(int *a, int *b);
+
 #endif /* MAIN_H */
 
