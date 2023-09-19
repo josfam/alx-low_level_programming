7511aea48796dee0c3f1facedc127046a1ab016a
josfam <josephamukun@tutanota.com>
2023-09-18 18:33:47 -0700

    Add implementation of `_puts`

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - Prints a string, followed by a new line.
+ * @str: The string to print.
+ * Description: Prints a string, followed by a new line to stdout.
+ * Return: Nothing.
+ */
+void _puts(char *str)
+{
+	int i;
+	int len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
