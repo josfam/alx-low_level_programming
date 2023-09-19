a85f7197265dba0e21433840d4777e505a91291e
josfam <josephamukun@tutanota.com>
2023-09-18 17:25:01 -0700

    Add signature for `_strlen`

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,5 +10,8 @@ void reset_to_98(int *n);
 /* Swaps the values of two integers. */
 void swap_int(int *a, int *b);
 
+/* Returns the length of a string. */
+int _strlen(char *s);
+
 #endif /* MAIN_H */
 
