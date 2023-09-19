2ea8f24e048e8ac9e56876d1b2994c4499c46dcc
josfam <josephamukun@tutanota.com>
2023-09-18 18:51:22 -0700

    Add implementation for `print_rev`

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse.
+ * @s: The string to print in reverse.
+ * Description: Prints a string, in reverse.
+ * Return: Nothing.
+ */
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = _strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
