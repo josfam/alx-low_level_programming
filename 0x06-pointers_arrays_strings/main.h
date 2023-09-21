cbc3d5b5d942e5b4be05bcbf2ad6090887ee9b3a
josfam <josephamukun@tutanota.com>
2023-09-21 21:49:11 +0300

    Add signature for `*_strcat`

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+/* Prints a char to the screen. */
+void _putchar(char ch);
+
+/* Concatenates two strings. */
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
+
