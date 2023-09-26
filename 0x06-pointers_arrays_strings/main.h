6da256236fac89d30996bdb3af0dead791e11eb7
josfam <josephamukun@tutanota.com>
2023-09-26 18:07:12 +0300

    Add signature for `reverse_array`

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,5 +16,8 @@ char *_strncpy(char *dest, char *src, int n);
 /* compares two strings */
 int _strcmp(char *s1, char *s2);
 
+/* Reverses the content of an array of integers. */
+void reverse_array(int *a, int n);
+
 #endif /* MAIN_H */
 
