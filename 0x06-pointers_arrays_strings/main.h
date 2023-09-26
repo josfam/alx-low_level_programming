d7a3dab5e6118de85452ba6704a91ebb758d2bb6
josfam <josephamukun@tutanota.com>
2023-09-26 13:43:58 +0300

    Add signature for `_strncat`

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,8 @@ void _putchar(char ch);
 /* Concatenates two strings. */
 char *_strcat(char *dest, char *src);
 
+/* Concatenates at most n bytes from source to destination string. */
+char *_strncat(char *dest, char *src, int n);
+
 #endif /* MAIN_H */
 
