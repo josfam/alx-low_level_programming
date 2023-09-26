a4dc50d327d7fa8a66fa78a0717f58150bdc7c9e
josfam <josephamukun@tutanota.com>
2023-09-26 17:59:26 +0300

    Use just `i` for indexing into both strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int s1i;
-	int s2i;
+	int i;
 
-	for (s1i = 0, s2i = 0; s1[s1i] != '\0' && s2[s2i] != '\0'; s1i++, s2i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[s1i] < s2[s2i])
+		if (s1[i] < s2[i])
 			return (-15);
-		if (s1[s1i] > s2[s2i])
+		if (s1[i] > s2[i])
 			return (15);
 	}
 	/* handle different lengths */
-	if (s1[s1i] == '\0' && s2[s2i] != '\0')
+	if (s1[i] == '\0' && s2[i] != '\0')
 		return (-15);
-	if (s1[s1i] != '\0' && s2[s2i] == '\0')
+	if (s1[i] != '\0' && s2[i] == '\0')
 		return (15);
 
 	/* They are the same */
