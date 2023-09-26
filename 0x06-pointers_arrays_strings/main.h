a4d0f9ffa393919b25bddf20d8f1e7dfff0af2c7
josfam <josephamukun@tutanota.com>
2023-09-26 16:44:53 +0300

    Add signature for `_strcmp`

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,8 @@ char *_strncat(char *dest, char *src, int n);
 /* Copies at most n bytes from source to destination string, akin to strncpy */
 char *_strncpy(char *dest, char *src, int n);
 
+/* compares two strings */
+int _strcmp(char *s1, char *s2);
+
 #endif /* MAIN_H */
 
