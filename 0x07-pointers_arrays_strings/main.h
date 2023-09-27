84a03ad12e1891795f34c06c47d87020eb3efb8c
josfam <josephamukun@tutanota.com>
2023-09-27 11:39:27 +0300

    Add signature for `_strchr`

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,8 @@ char *_memset(char *s, char b, unsigned int n);
 /* Copies memory area */
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/* Locates a character in a string. */
+char *_strchr(char *s, char c);
+
 #endif /* MAIN_H */
 
