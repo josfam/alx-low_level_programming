a58c8bcc23d3b9ee6a09cb13b7d1090fabc1e50b
josfam <josephamukun@tutanota.com>
2023-09-27 10:58:59 +0300

    Add signature for `_memset`

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+/* Prints a char to the screen. */
+void _putchar(char ch);
+
+/* Fills memory with a constant byte. */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
+
