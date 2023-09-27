16794f02b732633bb7ddaf713460b5e3d0ff0325
josfam <josephamukun@tutanota.com>
2023-09-27 11:29:19 +0300

    Add signature for `_memcpy`

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,8 @@ void _putchar(char ch);
 /* Fills memory with a constant byte. */
 char *_memset(char *s, char b, unsigned int n);
 
+/* Copies memory area */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 #endif /* MAIN_H */
 
