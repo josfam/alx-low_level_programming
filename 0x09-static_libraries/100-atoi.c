e400b6c1e2d0e618dad452c0fb2d367cb3a470ac
josfam <josephamukun@tutanota.com>
2023-10-08 15:01:33 +0300

    Add full and partial implementations of functions

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string into an integer.
+ * @s: The string to convert.
+ * Description: Converts a string into an integer.
+ * Return: The integer that is the result of string conversion.
+ */
+int _atoi(char *s)
+{
+	/* TODO */
+}
