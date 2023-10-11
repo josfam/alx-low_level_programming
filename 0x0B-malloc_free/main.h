b920960054837507e6e72ef555251ddac8d35844
josfam <josephamukun@tutanota.com>
2023-10-11 10:37:19 +0300

    Add signature for `create_array`

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,4 +4,7 @@
 /* Writes a character to screen. */
 void _putchar(char ch);
 
+/* Creates an array of chars, and initializes it with a specific char. */
+char *create_array(unsigned int size, char c);
+
 #endif /* MAIN_H */
