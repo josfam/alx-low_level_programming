6f786957e3bed518dd4da1d4df6c3bc66a2be17b
josfam <josephamukun@tutanota.com>
2023-10-15 04:19:53 +0300

    Add code for `1. To be is to be the value of a...`

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,4 +7,7 @@ int _putchar(char c);
 /* Returns the sum of all its parameters. */
 int sum_them_all(const unsigned int n, ...);
 
+/* Prints numbers, followed by a new line. */
+void print_numbers(const char *separator, const unsigned int n, ...);
+
 #endif
