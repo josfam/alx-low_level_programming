c2cd5e04d84cfcddce18ff7765c149398d9b062d
josfam <josephamukun@tutanota.com>
2023-10-15 04:23:39 +0300

    Fix undefined behavior with NULL separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(nums, int));
 
-		if (i < n - 1)
+		if (!(separator == NULL) && (i < n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
