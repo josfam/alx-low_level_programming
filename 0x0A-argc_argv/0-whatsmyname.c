7e370345501ed58ebe07bfcf5b373ae93fbd59a5
josfam <josephamukun@tutanota.com>
2023-10-27 10:45:55 +0300

    Add code for `0. It ain't what they call you, ...`

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * @argc: Length of array of commandline arguments.
+ * @argv: Array of commandline arguments.
+ * Description: The main entry point of the program.
+ * Return: 0 if successful, Non-zero otherwise.
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
