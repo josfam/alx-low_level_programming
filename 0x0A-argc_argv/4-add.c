132b2b3b53e284b4483142ae983d6edb9400e7ea
josfam <josephamukun@tutanota.com>
2023-10-28 05:09:39 +0300

    Move symbol checking and sum to functions

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+
+int containsSymbols(int argc, char **argv);
+int getSum(int argc, char **argv);
 
 /**
  * main - Entry point.
@@ -10,42 +12,74 @@
  * Description: The main entry point of the program.
  * Return: 0 if successful, Non-zero otherwise.
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	int i;
-	int j;
-	char *item;
-	int item_len;
-	int sum;
-
-	/* "0" if no numbers are passed */
+	/* Handle no number being passed */
 	if (argc == 1)
 	{
-		printf("%d\n", 0);
+		printf("0\n");
 	}
 	else
 	{
-		/* start adding numbers, and stop if a non-number is found */
-		for (i = 1; i < argc; i++)
+		if (containsSymbols(argc, argv))
 		{
-			item = argv[i];
-			item_len = strlen(item);
-
-			/* check the string for non-numeric characters*/
-			for (j = 0; j < item_len; j++)
-			{
-				if (!isdigit(item[j]))
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-
-			sum += atoi(item);
+			printf("Error\n");
+			return (1);
 		}
-
-		printf("%d\n", sum);
+		/* Get the sum of the numbers */
+		printf("%d\n", getSum(argc, argv));
 	}
 
 	return (0);
+}
+
+/**
+ * containsSymbols - Checks whether any of the commandline options contains
+ * a non-numeric symbol.
+ * @argc: Length of array of commandline arguments.
+ * @argv: Array of commandline arguments.
+ * Description: Checks whether any of the commandline options contains
+ * a non-numeric symbol
+ * Return: 0 if there are no non-numeric symbols found, 1 otherwise.
+ */
+int containsSymbols(int argc, char **argv)
+{
+	int i, j, strLen;
+	char symbol;
+
+	for (i = 1; i < argc; i++)
+	{
+		strLen = strlen(argv[i]);
+		for (j = 0; j < strLen; j++)
+		{
+			symbol = argv[i][j];
+			if (symbol < 48 || symbol > 57)
+			{
+				return (1);
+			}
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * getSum - Returns the sum of the numbers passed as commandline arguments.
+ * @argc: Length of array of commandline arguments.
+ * @argv: Array of commandline arguments.
+ * Description: Returns the sum of the numbers passed as commandline arguments.
+ * Return: The sum of the numbers passed as commandline arguments.
+ */
+int getSum(int argc, char **argv)
+{
+	int i, sum;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+
+	return (sum);
 }
