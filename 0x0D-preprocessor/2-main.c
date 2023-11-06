880544111541618f52e6e6821920fab00160670c
josfam <josephamukun@tutanota.com>
2023-11-07 02:38:16 +0300

    Add code for `2. File name`

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * @argc: Length of array of commandline arguments.
+ * @argv: Array of commandline arguments.
+ * Description: The main entry point of the program.
+ * Return: 0 if successful, Non-zero otherwise.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
