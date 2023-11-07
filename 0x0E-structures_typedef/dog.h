6b697146bcb4e19c88408b514006ae76bb365d8a
josfam <josephamukun@tutanota.com>
2023-11-07 12:02:57 +0300

    Add code for `0. Poppy`

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+
+/**
+ * struct dog - Represents a dog
+ * @name: The dog's name.
+ * @age: The dog's age.
+ * @owner: The dog's owner.
+ *
+ * Description: Represents a dog with a name, age, and an owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
