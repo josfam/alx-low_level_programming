16c0d9f63e2b8fcac69aa304ebdf8b2acafee707
josfam <josephamukun@tutanota.com>
2023-11-10 00:03:00 +0300

    Add code for `0. Beauty is variable, ugliness ...`

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of all its parameters.
+ * @n: The number of parameters to add.
+ * Description: Returns the sum of all its parameters.
+ * Return: The sum of the parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list numargs;
+
+	if (n == 0)
+		return 0;
+
+	va_start(numargs, n);
+	sum = 0;
+
+	for(i = 0; i < n; i++)
+	{
+		sum += va_arg(numargs, int);
+	}
+
+	va_end(numargs);
+
+	return sum;
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,7 @@
 #ifndef VARIADIC_HEADER
 #define VARIADIC_HEADER
 
+/* Returns the sum of all its parameters */
+int sum_them_all(const unsigned int n, ...);
+
 #endif
