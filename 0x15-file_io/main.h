4c99f8a5814f8cf088691f2adb7678069f30b18b
josfam <josephamukun@tutanota.com>
2023-12-01 01:32:12 +0300

    Add starter files

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+#include <stddef.h>
+#include <unistd.h>
+
+/* Reads a text file and prints it to the POSIX standard output. */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
