e534afb12e9559689824fe18ad9b8043fda953c5
josfam <josephamukun@tutanota.com>
2023-12-01 12:24:11 +0300

    Add _putchar.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,4 +7,7 @@
 /* Reads a text file and prints it to the POSIX standard output. */
 ssize_t read_textfile(const char *filename, size_t letters);
 
+/* Writes a character to the screen. */
+int _putchar(char ch);
+
 #endif
