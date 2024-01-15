27ad192268532329bd4729e905b16a5438d28ad2
josfam <josephamukun@tutanota.com>
2024-01-15 11:06:29 +0300

    Formatting: Fix function spaces

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,7 +8,7 @@
 * Description: Returns the number of elements in a linked dlistint_t list.
 * Return: The number of elements in a linked dlistint_t list.
 */
-size_t dlistint_len (const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
 	const dlistint_t *current;
