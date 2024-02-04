159627d29b9d0f0724a1d44dd6da4d4ba2da0660
josfam <josephamukun@tutanota.com>
2024-02-04 17:31:48 +0300

    Implement `3. >>> ht['betty'] = 'cool'`

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "hash_tables.h"
+
+/**
+* hash_table_set - Adds an element to the hash table
+* @ht: The hash table to add or update the key/value to
+* @key: The key to use. Cannot be an empty string
+* @value: The value associated with the key. Can be an empty string
+* Description: Adds an element to the hash table
+* Return: 1 if succeeded, 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	unsigned long int table_size;
+	hash_node_t *new_node, *current;
+
+	/* calculate the index to put the value */
+	table_size = ht->size;
+	index = key_index((const unsigned char *)key, table_size);
+
+	/* create a hash node to store the key and value */
+	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = (char *)key;
+	new_node->value = (char *)value;
+	new_node->next = NULL;
+
+	current = ht->array[index];
+
+	/* if index is unoccupied, add the new_node at that index */
+	if (!current)
+	{
+		ht->array[index] = new_node;
+	}
+	else
+	{
+		new_node->next = current;
+		ht->array[index] = new_node;
+	}
+	return (1);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -39,4 +39,7 @@ unsigned long int hash_djb2(const unsigned char *str);
 /* Gives the index of a key */
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
+/* Adds an element to the hash table */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
 #endif
