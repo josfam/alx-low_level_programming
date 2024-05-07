d84bc2e98d23abe556787d4821e7b2839b801a64
josfam <jamukun@proton.me>
2024-05-07 14:52:51 +0300

    Implement `0. Linear search`

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_HEADER
+#define SEARCH_ALGOS_HEADER
+
+#include <stddef.h>
+
+/* Searches for a value in an array of integers using the Linear search */
+/* algorithm */
+int linear_search(int *array, size_t size, int value);
+
+#endif
